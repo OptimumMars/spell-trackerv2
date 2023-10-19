33d61a987f7671a7818356a9f6a5819c9b6366c8
OptimumMars <gabriel.gutz@live.com>
2023-10-18 22:59:54 -0400

    added user/session routes

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,6 +1,13 @@
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 const { restoreUser } = require("../../utils/auth.js");
+
 router.use(restoreUser);
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
 
 // GET /api/set-token-cookie
 //this tests our authentication middleware
