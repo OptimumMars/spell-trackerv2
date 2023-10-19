e06661d356c33f65360fc67c518f7a46d819fa38
OptimumMars <gabriel.gutz@live.com>
2023-10-18 20:28:59 -0400

    added auth util functions and test routes in api index

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,42 @@
 const router = require('express').Router();
+const { restoreUser } = require("../../utils/auth.js");
+router.use(restoreUser);
 
-// router.post('/test', function(req, res) {
-//     res.json({ requestBody: req.body });
+// GET /api/set-token-cookie
+//this tests our authentication middleware
+// const { setTokenCookie } = require('../../utils/auth.js');
+// const { User } = require('../../db/models');
+// router.get('/set-token-cookie', async (_req, res) => {
+//   const user = await User.findOne({
+//     where: {
+//       username: 'Demo-lition'
+//     }
 //   });
+//   setTokenCookie(res, user);
+//   return res.json({ user: user });
+// });
+
+// GET /api/restore-user
+// const { restoreUser } = require('../../utils/auth.js');
+
+// router.use(restoreUser);
+
+// router.get(
+//   '/restore-user',
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+// GET /api/require-auth
+// const { requireAuth } = require('../../utils/auth.js');
+// router.get(
+//   '/require-auth',
+//   requireAuth,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
 
 //example fetch for testing api routes in the browser after going to /api/csrf/restore
 // fetch('/api/test', {
@@ -13,5 +47,9 @@ const router = require('express').Router();
 //     },
 //     body: JSON.stringify({ hello: 'world' })
 //   }).then(res => res.json()).then(data => console.log(data));
+
+/* end of test routes */
+
+
 
 module.exports = router;
