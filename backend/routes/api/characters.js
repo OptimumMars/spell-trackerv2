e37dbfb4f062ab547bcb580dcbb86381651e6894
OptimumMars <gabriel.gutz@live.com>
2023-10-24 20:24:20 -0400

    added get route for all characters

--- backend/routes/api/characters.js
+++ backend/routes/api/characters.js
@@ -3,6 +3,7 @@ const { Character } = require('../../db/models');
 
 const router = express.Router();
 
+//Create a new character
 router.post("/", async (req, res) => {
     let { name, race, userId } = req.body;
 
@@ -13,7 +14,16 @@ router.post("/", async (req, res) => {
         class: req.body.class
     });
 
+    res.status(201)
     res.json(newCharacter)
 });
+
+//Get all characters
+router.get("/", async (req, res) => {
+    const allCharacters = await Character.findAll();
+
+    res.status(200);
+    res.json(allCharacters);
+})
 
 module.exports = router;
