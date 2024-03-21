b30540a44512cfe50d74021747fcafe80ce4f5f2
OptimumMars <gabriel.gutz@live.com>
2024-03-21 19:53:41 -0400

    reconfigured redux store to change to a single character when on a details page

--- frontend/src/components/CharacterDetails/index.js
+++ frontend/src/components/CharacterDetails/index.js
@@ -2,11 +2,21 @@ import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 
+import { getSingleCharacter } from "../../store/character";
+
 function CharacterDetails() {
     const dispatch = useDispatch();
     let characterId = useParams().characterId
-    console.log(characterId)
-    let character = useSelector((state) => state.character[characterId])
+    let character = useSelector((state) => state.character)
+    // console.log(character)
+
+    useEffect(() => {
+        async function fetchCharacter(){
+            await dispatch(getSingleCharacter(characterId))
+        }
+
+        fetchCharacter();
+    }, [dispatch, characterId])
 
     console.log(character);
 
--- frontend/src/store/character.js
+++ frontend/src/store/character.js
@@ -1,11 +1,19 @@
 import { csrfFetch } from "./csrf";
 
 const SET_CHARACTERS = "characters/setCharacters";
+const SET_CHARACTER = "characters/setCharacter";
 
 const setCharacters = (characters) => {
   return {
     type: SET_CHARACTERS,
     payload: characters,
+  };
+};
+
+const setCharacter = (character) => {
+  return {
+    type: SET_CHARACTER,
+    payload: character,
   };
 };
 
@@ -16,6 +24,18 @@ export const getCharacters = (userId) => async (dispatch) => {
     const characters = await response.json();
 
     dispatch(setCharacters(characters));
+  }
+};
+
+export const getSingleCharacter = (characterId) => async (dispatch) => {
+  const response = await csrfFetch(`/api/characters/${characterId}`);
+
+  if (response.ok) {
+    const character = await response.json();
+
+    console.log(character);
+
+    dispatch(setCharacter(character));
   }
 };
 
@@ -30,6 +50,10 @@ const characterReducer = (state = initialState, action) => {
         newState[character.id] = character;
       });
       //   newState.characters = action.payload;
+      return newState;
+
+    case SET_CHARACTER:
+      newState = action.payload;
       return newState;
     default:
       return state;
